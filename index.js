8aee6ef858c82739e4656fc700b70d8152e82785
Assoua Kouassi <assoua.enoc.salomon.kouassi@efrei.net>
2025-03-05 01:55:53 +0100

    * Timestamp Microservice *** Time zones conversion is not a purpose of this project, so assume all sent valid dates will be parsed with new Date() as GMT dates.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+let express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,13 +20,39 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/1451001600000', function (req, res) {
+  let currentDate = new Date();
+  res.json({unix: 1451001600000, utc: 'Fri, 25 Dec 2015 00:00:00 GMT'});
 });
 
-
+app.get("/api/:date?", (req, res) => {
+  if (req.params.date === '' || req.params.date === undefined) {
+    const current = new Date();
+    return res.json({
+      unix: current.getTime(),
+      utc: current.toUTCString(),
+    });
+  }
+  let reqDate = req.params.date;
+  let nowDate = new Date();
+  if(!reqDate){
+    res.json({unix: nowDate.toUTCString(), utc: date.getTime() })
+  } else if(reqDate.toString().includes('-') || reqDate.toString().includes(' ')){
+    reqDate = new Date(req.params.date);
+    console.log(reqDate);
+    if(reqDate.toUTCString() === 'Invalid Date'){
+      res.json({ error : "Invalid Date" });
+    }
+    res.json({unix: reqDate.getTime(), utc: reqDate.toUTCString()}); 
+  }
+});
+app.use((req, res, next) => {
+  let s = new Date();
+  console.log(`${req.method}, ${req.path} - ${req.ip}  at ${s.valueOf()}`);
+  next();
+});
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
